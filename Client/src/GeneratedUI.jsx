46a3ed2b6b282514455052bc54a80f7087739354
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-04-04 22:52:02 +0530

    admin can now create new components

--- Client/src/GeneratedUI.jsx
+++ Client/src/GeneratedUI.jsx
@@ -5,15 +5,7 @@ export default function GeneratedUI() {
   return (
     <>
       <div className="p-4 min-h-screen">
-<div className="p-4 border">
-
-</div>
-<Button text="Click Me" className="bg-blue-500 text-white px-4 py-2" />
-<Button text="Click Me" className="bg-blue-500 text-white px-4 py-2" />
-<Button text="Click Me" className="bg-blue-500 text-white px-4 py-2" />
-<Button text="Click Me" className="bg-blue-500 text-white px-4 py-2" />
-<Button text="Click Me" className="bg-blue-500 text-white px-4 py-2" />
-<Button text="Click Me" className="bg-blue-500 text-white px-4 py-2" />
+<Button text="New Component" className="p-2 border" />
 </div>
     </>
   );
