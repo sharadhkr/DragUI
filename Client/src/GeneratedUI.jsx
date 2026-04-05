99f2cc4422f238a7518917cdf0a227bffb854a59
sharad.hkr <www.sharadrathoreji@gmail.com>
2026-04-05 15:57:34 +0530

    new compos

--- Client/src/GeneratedUI.jsx
+++ Client/src/GeneratedUI.jsx
@@ -1,18 +1,11 @@
 
-import { Button } from "DropUi";
+import {  } from "DropUi";
 
 export default function GeneratedUI() {
   return (
     <>
       <div className="p-4 min-h-screen">
-<Button text="New Component" className="p-2 border" />
 
-
-
-
-
-
-<Button text="New Component" className="p-7 border" />
 </div>
     </>
   );
